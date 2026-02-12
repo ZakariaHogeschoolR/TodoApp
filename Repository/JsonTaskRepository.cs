b35127a69ca0e4acb2de4406842addccb474a7c5
ZakariaHogeschoolR <ztahiri26@gmail.com>
2026-02-12 18:00:35 +0100

    walk true

---
+++ Repository/JsonTaskRepository.cs
@@ -0,0 +1,25 @@
+using System.Text.Json;
+using Microsoft.VisualBasic;
+using Model;
+
+public class JsonTaskRepository: ITaskRepository 
+{
+    private readonly string _filePath;
+    public JsonTaskRepository(string filePath) => _filePath = filePath;
+    public List<TaskItem> LoadTasks()
+    {
+        if(!File.Exists(_filePath))
+        {
+            return new List<TaskItem>();
+        }
+        string json = File.ReadAllText(_filePath);
+        var tasks = JsonSerializer.Deserialize<List<TaskItem>>(json);
+        return tasks ?? new List<TaskItem>();
+    }
+    public void SaveTasks(List<TaskItem> tasks)
+    {
+        string json = JsonSerializer.Serialize(tasks, new
+            JsonSerializerOptions { WriteIndented = true });
+                File.WriteAllText(_filePath, json); 
+    }
+}
