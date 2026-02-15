04cd8355d8ae24c6ab4520c4498f28aa7e5bf704
ZakariaHogeschoolR <ztahiri26@gmail.com>
2026-02-16 00:45:35 +0100

    everything works with arrays now we have to add some interface functions in IMyCollection these are commented out

--- Repository/RepositoryInterface/ITaskRepository.cs
+++ Repository/RepositoryInterface/ITaskRepository.cs
@@ -2,6 +2,6 @@ using System.Runtime.CompilerServices;
 using Model;
 public interface ITaskRepository
 {
-    List<TaskItem> LoadTasks();
-    void SaveTasks(List<TaskItem> tasks);
+    TaskItem[] LoadTasks();
+    void SaveTasks(TaskItem[] tasks);
 }
--- Service/TaskService.cs
+++ Service/TaskService.cs
@@ -1,9 +1,11 @@
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
 using Model;
 
-public class TaskService: ITaskService
+public class TaskService: ITaskService, IMyCollection<TaskItem>//, IMyIterator<TaskItem>
 {
     private readonly ITaskRepository _repository;
-    private readonly List<TaskItem> _tasks;
+    private TaskItem[] _tasks;
     public TaskService(ITaskRepository repository)
     {
         _repository = repository;
@@ -11,29 +13,111 @@ public class TaskService: ITaskService
     }
 
     public IEnumerable<TaskItem> GetAllTasks() => _tasks;
+    
     public void AddTask(string description)
     {
-        int newId = _tasks.Count > 0 ? _tasks[_tasks.Count - 1].Id + 1 : 1;
-        var newTask = new TaskItem { Id = newId, Description = description, Completed = false };
-        _tasks.Add(newTask);
-        _repository.SaveTasks(_tasks);
+        int id = _tasks.Length + 1;
+        TaskItem newTask = new TaskItem
+        {
+            Id = id, 
+            Description = description, 
+            Completed = false
+        };
+        Add(newTask);
     }
+
+    public void Add(TaskItem item)
+    {
+        int size = _tasks.Length + 1;
+        TaskItem[] newTaskArray = new TaskItem[size];
+        for(int i = 0; i < newTaskArray.Length; i++)
+        {
+            if(i > _tasks.Length - 1)
+            {
+                newTaskArray[size - 1] = item;
+                break;
+            }
+            else
+            {
+                newTaskArray[i] = _tasks[i];
+            }
+        }
+        _tasks = newTaskArray;
+        _repository.SaveTasks(newTaskArray);
+    }
+
+    public TaskItem FindBy<K>(K key, Func<TaskItem, K, bool> comparer)
+    {
+        for(int i = 0; i < _tasks.Length; i++)
+        {
+            if(comparer(_tasks[i], key))
+            {
+                return _tasks[i];
+            }
+        }
+        return null;
+    }
+
     public void RemoveTask(int id)
     {
-        var task = _tasks.Find(t => t.Id == id);
+        var task = FindBy(id, (t, id) => t.Id == id);
         if(task != null)
         {
-            _tasks.Remove(task);
-            _repository.SaveTasks(_tasks);
+            Remove(task);
         }
+    }
+
+    public void Remove(TaskItem Item)
+    {
+        if(Item == null) return;
+
+        int index = -1;
+
+        for (int i = 0; i < _tasks.Length; i++)
+        {
+            if (_tasks[i] == Item)
+            {
+                index = i;
+                break;
+            }
+        }
+        if(index == -1) return;
+
+        TaskItem[] newArray = new TaskItem[_tasks.Length - 1];
+        
+        for (int i = 0, j = 0; i < _tasks.Length; i++)
+        {
+            if (i == index) continue; // overslaan
+            newArray[j] = _tasks[i];
+            j++;
+        }
+        _tasks = newArray;
+        _repository.SaveTasks(newArray);
     }
 
     public void ToggleTaskCompletion(int id)
     {
-        var task = _tasks.Find(t => t.Id == id);
+
+        var task = FindBy(id, (t, id) => t.Id == id);
         if (task != null) {
-            task.Completed = !task.Completed;
-            _repository.SaveTasks(_tasks);
+            int index = -1;
+            for(int i = 0; i < _tasks.Length - 1; i++)
+            {
+                if(_tasks[i].Id == id)
+                {
+                    index = i;
+                    if(_tasks[index].Completed)
+                    {
+                        _tasks[index].Completed = false;
+                    }
+                    else
+                    {
+                        _tasks[index].Completed = true;
+                    }
+                    _repository.SaveTasks(_tasks);
+                    break;
+                }
+            }
         }
     }
 }
